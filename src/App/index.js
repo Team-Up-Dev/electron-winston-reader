eb1148e3d4164adbf20250cdc784e4710e73f1e3
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-18 21:46:39 +0700

    Move App.js into separate folder

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col>
+          <h1 className="text-center">Insert File</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
