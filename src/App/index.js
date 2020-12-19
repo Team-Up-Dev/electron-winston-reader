820b974db4e00bc491399ff97252f27014ad75fc
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2020-12-19 19:52:22 +0700

    Add button

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 function App() {
   return (
@@ -6,6 +6,16 @@ function App() {
       <Row className="justify-content-center">
         <Col>
           <h1 className="text-center">Insert File</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col>
+          <Button
+            variant="primary"
+            onClick={() => window.ipcRenderer.send("file:request")}
+          >
+            Choose File
+          </Button>
         </Col>
       </Row>
     </Container>
